63969dbe36376a8cf84d4b31b1242a8502894b67
Raul Jordan <rauljordan@college.harvard.edu>
2015-04-12 17:52:59 -0400

    fixed for heroku 3

--- formatter.py
+++
@@ -1,9 +0,0 @@
-
-class Formatter(object):
-	"""Base formatter class that enforces datatype to
-	be a specific JSON object according to a schema"""
-	def __init__(self):
-		self.schema = {
-			'item': 'item'
-		}
-		
