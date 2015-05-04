262cb7ebda0ba7271369621eefbd5a8944dcc96a
Claudia Huang <claudiahuang@college.harvard.edu>
2015-05-04 11:39:15 -0400

    tried to fix nodes on force graph

--- static/js/force.js
+++ static/js/force.js
@@ -1,4 +1,3 @@
-
 /**
  * Force object for Pycoin
  * @param _parentElement -- the HTML or SVG element (D3 node) to which to attach the vis
@@ -13,13 +12,9 @@ Force = function(_parentElement, _nodeData, _eventHandler){
     this.displayData = [];
     var style = window.getComputedStyle(this.parentElement.node(), null);
 
-    this.margin = {top: 20, right: 50, bottom: 30, left: 20},
-    this.width = parseInt(style.getPropertyValue('width')) - this.margin.left - this.margin.right;
-<<<<<<< HEAD
-    this.height = 800 - this.margin.top - this.margin.bottom;
-=======
-    this.height = 1200 - this.margin.top - this.margin.bottom;
->>>>>>> origin/claudia_branch
+    this.margin = {top: 20, right: 50, bottom: 10, left: 20},
+    this.width = parseInt(style.getPropertyValue('width')) - this.margin.left - this.margin.right;
+    this.height = 1200 - this.margin.top - this.margin.bottom;
 
     this.initVis();
 }
@@ -67,6 +62,7 @@ Force.prototype.initVis = function(){
 
 	this.link = this.svg.selectAll(".link");
     this.node = this.svg.selectAll(".node");
+    this.radius = 10;
 
 
 	this.svg.selectAll('text.label').attr("fill", "black")
@@ -74,60 +70,6 @@ Force.prototype.initVis = function(){
 	// Makes the force layout
 	this.makeForce(this.graph);
 	
-
-    /*
-    this.link = this.svg.selectAll(".link")
-              		.data(this.graph.links);
-
-	this.link.enter().append("line")
-	    .attr("class", "link")
-
-	this.link.
-		on('mouseover', function(d) {
-			d3.select(this).style({'stroke':'red', 'stroke-width':'15px'});
-			var name = d.source.name + '/' + d.target.name;
-			$(that.eventHandler).trigger("selectionChanged", name);
-				
-		})
-		.on('mouseout', function(d) {
-			
-			d3.select(this).style({'stroke':'#008894','stroke-width':'2px'});
-		});
-
-	this.node = this.svg.selectAll(".node")
-	              .data(this.graph.nodes)
-	              .enter()
-	              .append("g").attr("class", "node")
-	              
-              	  
-
-	this.svg.selectAll('text.label').attr("fill", "black")
-
-	this.force = d3.layout.force()
-	    .size([this.width, this.height])
-	    .charge(-400)
-	    .linkDistance(40)
-	    .on('tick', this.tick)
-
-
-	this.drag = this.force.drag()
-    	.on("dragstart", this.dragstart);
-
-   	this.force
-      .nodes(this.graph.nodes)
-      .links(this.graph.links)
-      .start();
-
-    this.node.append("circle")
-	    .attr("r", 7)
-
-	this.node.append("text")
-				  .attr("dx", ".80em")
-				  .attr("dy", ".10em")
-		          .text(function(d) { return d.name; })
-		          .on('dblclick', this.dblclick)
-		          .call('drag');
-  	*/
 };
 
 
@@ -135,16 +77,56 @@ Force.prototype.makeForce = function(theGraph) {
 	var that = this;
 	this.force = d3.layout.force()
 	    .size([this.width, this.height])
-	    .charge(-100)
-	    .linkDistance(300)
+	    .gravity(0.1)
+	    .charge(-300)
+	    .linkDistance(80)
 	    .on("tick", function() {
 	    	  that.link.attr("x1", function(d) { return d.source.x; })
 			      .attr("y1", function(d) { return d.source.y; })
 			      .attr("x2", function(d) { return d.target.x; })
 			      .attr("y2", function(d) { return d.target.y; });
 
-			  that.node.attr("cx", function(d) { return d.x; })
-			      .attr("cy", function(d) { return d.y; });
+			 
+			  that.node.attr("cx", function(d) { 
+
+			  		if (d.name == "BTC") {
+			  			d3.select(this).classed("fixed", d.fixed = true);
+			  			return d.x = 225;
+			  		}
+			  		else if (d.name == "LTC") {
+			  			d3.select(this).classed("fixed", d.fixed = true);
+			  			return d.x = 250;
+			  		}
+			  		else if (d.name == "XRP") {
+			  			d3.select(this).classed("fixed", d.fixed = true);
+			  			return d.x = 275;
+			  		}			  		
+			  		else {
+			  			return d.x = Math.max(that.radius, 
+			  				Math.min(that.width - that.radius, d.x)); 
+			  		}
+			  		
+			  	})
+        		.attr("cy", function(d) { 
+        			
+			  		if (d.name == "BTC") {
+			  			d3.select(this).classed("fixed", d.fixed = true);
+			  			return d.y = 150;
+			  		}
+			  		else if (d.name == "LTC") {
+			  			d3.select(this).classed("fixed", d.fixed = true);
+			  			return d.y = 50;
+			  		}
+			  		else if (d.name == "XRP") {
+			  			d3.select(this).classed("fixed", d.fixed = true);
+			  			return d.y = 100;
+			  		}			  		
+			  		else {
+			  			return d.y = Math.max(that.radius, 
+			  				Math.min(that.width - that.radius, d.y)); 
+			  		}
+        		 
+        		});
 	    });
 
 	this.drag = this.force.drag()
@@ -160,28 +142,33 @@ Force.prototype.makeForce = function(theGraph) {
       .attr("class", "link");
 
   this.node = this.node.data(theGraph.nodes)
-    .enter().append("circle")
+      .enter()
+      .append("circle")
       .attr("class", "node")
-      .attr("r", 12)
+      .attr("r", this.radius - 0.75)
       .on("dblclick", this.dblclick)
       .call(this.drag)
 
-<<<<<<< HEAD
-  this.node.append("text")
-	  .attr("dx", ".80em")
-	  .attr("dy", ".10em")
-      .text(function(d) { return d.name; });
-=======
   	
    this.link.
 		on('mouseover', function(d) {
-			
+
 			d3.selectAll(".link").style({'opacity':'0.1'});
 			d3.select(this).style({'stroke':'red', 'stroke-width':'15px', 'opacity':'1'});
 
 			var name = d.source.name + '/' + d.target.name;
 		
 			$(that.eventHandler).trigger("selectionChanged", name);
+
+
+			$("#img1").html('<h1>Hello World </h1>');
+			$("#img2").html('');
+
+			var src1 = "{{url_for('static', filename='img/coins/" + d.source.name + ".png')}}";
+			var src2 = "{{url_for('static', filename='img/coins/" + d.target.name + ".png')}}";
+			$('#img1').html('<img src=' + src1 + ' class="activator">')
+			$('#img2').html('<img src=' + src2 + ' class="activator">')
+
 						
 				
 		})
@@ -192,7 +179,6 @@ Force.prototype.makeForce = function(theGraph) {
 				'stroke-width':'2px',
 				'opacity':'1'
 			});
->>>>>>> origin/claudia_branch
 
 
 		});
@@ -222,4 +208,3 @@ Force.prototype.tick = function() {
   this.node.attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; });
 };
-
